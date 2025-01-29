ea34536f6cb1540e546c9699c938bad7892661e6
jsingh2704 <juliesingh2704@gmail.com>
2025-01-29 12:00:19 +0530

    Initial commit with React assignment

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("This is a simple and professional page.");
+
+  const handleClick = () => {
+    setMessage("You clicked the button!");
+  };
+
+  return (
+    <div className="container">
+      <h1>Welcome to My Assignment</h1>
+      <p>{message}</p>
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
